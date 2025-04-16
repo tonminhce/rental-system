5374cfb73509a881bb18d627aeb661cb51ababae
wenviva <quocanh010104@gmail.com>
2025-04-16 17:28:26 +0700

    [Feature] configure JwtModule for token signing

--- backend/src/modules/common/auth/auth.module.ts
+++ backend/src/modules/common/auth/auth.module.ts
@@ -3,11 +3,22 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './strategies/jwt-token.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [JwtModule],
+  imports: [JwtModule.registerAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: async (configService: ConfigService) => ({
+      secret: configService.get<string>('TOKEN_SECRET'),
+      signOptions: {
+        expiresIn: configService.get<string>('TOKEN_EXPIRATION'),
+      },
+    }),
+  }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
   exports: [JwtModule],
 })
-export class AuthModule {}
+export class AuthModule { }
