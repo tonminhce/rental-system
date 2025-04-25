505ba17bb52a9e50fde3b3863e0de042fdd401ff
wenviva <quocanh010104@gmail.com>
2025-04-25 16:10:38 +0700

    [Develop]: Add env variable SWAGGER_PATH + Add ApiResponse, ApiOperation for Signup Controller and Login Controller + Add ApiProperty for LoginDTO and SignupDTO

--- backend/src/modules/auth/dto/login.dto.ts
+++ backend/src/modules/auth/dto/login.dto.ts
@@ -1,11 +1,14 @@
 import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginDto {
+  @ApiProperty({ example: 'user@example.com' })
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({ example: 'yourPassword123', minLength: 6 })
   @IsNotEmpty()
   @MinLength(6)
   password: string;
-} 
+}
